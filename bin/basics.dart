f3928a80426be9b763ab817fa4600603bcb94409
Saranjith <saranjith@metrictreelabs.com>
2023-04-05 21:33:57 +0530

    array

--- bin/basics.dart
+++ bin/basics.dart
@@ -36,7 +36,56 @@ p
   }
 
 //loop
-for (var i=0;i<=10;i++){
-  print(i)
-}
+  for (var i = 0; i <= 10; i++) {
+    print(i);
+  }
+
+//array || list
+  List<int> numberList = [1, 2, 3, 5];
+
+// ||
+
+  var array = [1, "hi", 5, "hello"];
+
+  print(numberList.length);
+
+  if (numberList.contains(3)) {
+    print('number list include 3');
+  }
+
+  numberList.add(50);
+  numberList.remove(4);
+  numberList.removeAt(4);
+
+  ///...
+  array.addAll(numberList);
+  print(numberList.join(',')); // print value with coma
+
+  List<List<int>> list = [
+    [1, 2, 3],
+    [3, 4, 5]
+  ];
+
+  print(list[0][1]); //0 th possition array first value
+
+  // remove duplicates
+
+  List<int> nuList = [1, 1, 2, 5];
+  Set<int> nuSet = {1, 1, 2, 5};
+
+  print(nuList);
+
+  //remove duplicate
+  print(nuSet);
+
+//map key with value
+  Map<String, String> maps = {"name": "saran", "lastname": "p", "age": "20"};
+
+  // ||
+
+  //no type required
+  var map = {"name": "saran", "lastname": "p", "age": 20};
+
+  print(maps['name']);
+  print(maps['age']);
 }
