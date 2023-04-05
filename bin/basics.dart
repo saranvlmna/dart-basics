17e20e0dc911cc2c5b3fb88078c6c29227ab7ef7
Saranjith <saranjith@metrictreelabs.com>
2023-04-05 21:07:51 +0530

    string

--- bin/basics.dart
+++ bin/basics.dart
@@ -14,5 +14,16 @@ void main() {
   var num1 = int.parse(input1!);
   var num2 = int.parse(input2!);
 
-  print('sum:${num1 +num2}');
+  print('sum:${num1 + num2}');
+
+  String name = 'saranjith';
+  String nameWithAddress = "I \"m Saranjith Palakkandy";
+  String multiLineString = '''
+my name 
+is 
+saranjith
+p
+''';
+
+  print(name.length);
 }
